7312de565f9d97535af9a44d062afb031df6ce84
Ausbel <ausbel.dev@gmail.com>
2026-09-24 23:11:50 -0600

    feat(sofle): track baseline via keymap with bongo cat

---
+++ keyboards/sofle/keymaps/via/config.h
@@ -0,0 +1,15 @@
+// Copyright 2024 Santosh Kumar (@santosh)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+#define DYNAMIC_KEYMAP_LAYER_COUNT 5
+
+#define TRI_LAYER_LOWER_LAYER 2
+#define TRI_LAYER_UPPER_LAYER 3
+#define TRI_LAYER_ADJUST_LAYER 4
+
+#define EE_HANDS
+#define SPLIT_USB_DETECT
+#define SPLIT_WPM_ENABLE
+#define SPLIT_OLED_ENABLE
