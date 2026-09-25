81d8b5bea1a71c9d3d302850eff94fb9e33274b0
Ausbel <ausbel.dev@gmail.com>
2026-09-24 21:32:55 -0600

    feat(sofle): add bongo cat oled animation and config

---
+++ keyboards/sofle/bongocat.h
@@ -0,0 +1,10 @@
+// Copyright 2024 QMK
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+#include "quantum.h"
+
+#ifdef OLED_ENABLE
+void render_bongocat(void);
+#endif
---
+++ keyboards/sofle/config.h
@@ -0,0 +1,7 @@
+// Copyright 2024 QMK
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+#define EE_HANDS
+#define SPLIT_USB_DETECT
